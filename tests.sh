318b82bf770f8b8b4c5b6770bcc4ac9724d56dc8
Simone <126069316+simo-corbo@users.noreply.github.com>
2024-05-22 15:27:05 +0200

    fix test

--- tests.sh
+++ tests.sh
@@ -6,7 +6,7 @@
 #max_mem_5_fs_glit
 
 # with mistral as sut and generator
-python evolutionary.py -i 10 -f dataset/reduced/questions_reduced10 -o results/finalTests/mistral_mistral/max_fs_glit.json -fs -g -sut mistral -sg mistral
-python evolutionary.py -i 10 -f dataset/reduced/questions_reduced10 -o results/finalTests/mistral_mistral/max_fs.json -fs -sut mistral -sg mistral
-python evolutionary.py -i 10 -f dataset/reduced/questions_reduced10 -o results/finalTests/mistral_mistral/max.json -sut mistral -sg mistral
-python evolutionary.py -i 10 -f dataset/reduced/questions_reduced10 -o results/finalTests/mistral_mistral/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut mistral -sg mistral
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max_fs_glit.json -fs -g -sut mistral -sg mistral
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max_fs.json -fs -sut mistral -sg mistral
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max.json -sut mistral -sg mistral
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut mistral -sg mistral
