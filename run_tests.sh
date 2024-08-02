1a60cf0f3f9d2de930126230a7f868b47bea2d97
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2024-08-02 12:16:41 +0200

    Clean repository for release

---
+++ run_tests.sh
@@ -0,0 +1,23 @@
+# Using the best configuration for the evolutionary algorithm
+
+#max_fs_glit
+#max_fs
+#max
+#max_mem_5_fs_glit
+
+# Exit on error and log commands
+set -xe
+
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_vicuna/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut vicuna -sg vicuna -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_vicunaUC/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut vicuna -sg vicunaUC -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/llama3_vicuna/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut llama3 -sg vicuna -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_mistral/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut vicuna -sg mistral -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_vicuna/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut mistral -sg vicuna -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut mistral -sg mistral -q -1 --api-key-perspective [PERSPECTIVE KEY]
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_vicuna/max.json -sut mistral -sg vicuna -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_vicunaUC/max.json -sut mistral -sg vicunaUC -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_vicuna/max.json -sut vicuna -sg vicuna -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_vicunaUC/max.json -sut vicuna -sg vicunaUC -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicunaUC_vicunaUC/max.json -sut vicunaUC -sg vicunaUC -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_mistral/max.json -sut mistral -sg mistral -q -1
+python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicuna_mistral/max.json -sut vicuna -sg mistral -q -1
--- tests2.sh
+++
@@ -1,6 +0,0 @@
-## Exit on error and log commands
-set -xe
-
-
-#python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/vicunaUC_vicunaUC/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut vicunaUC -sg vicunaUC -q -1 --api-key-perspective [PERSPECTIVE KEY]
-python evolutionary.py -i 10 -f dataset/reduced/questions_reduced100 -o results/finalTests/mistral_vicunaUC/max_mem_5_fs_glit.json -fs -g -m -w 5 -sut mistral -sg vicunaUC -q -1 --api-key-perspective [PERSPECTIVE KEY]
