bc3128c79f71c7806d63876aa2f8185141f15cf1
Simone <126069316+simo-corbo@users.noreply.github.com>
2024-11-19 15:47:49 +0100

    add perplexity results

--- tools/perplexityPlotter.py
+++ tools/perplexityPlotter.py
@@ -17,31 +17,51 @@ def sortValues(data):
     # data contains the list of files to read
     # return the dataframe
     values = []
+    baselineValues = []
     for file in data:
         with open(file, 'r') as f:
             data = json.load(f)
-            sut = data['sut']#+'/'+data['configuration']
-            configuration = data['configuration']
+            sut = data['config']['systemUnderTest']#+'/'+data['configuration']
+            
+            handle = data['handle']
             model = data['model']
-            for run in data['runs']:
-                values.append([run['initial']['ppl'], 0, sut, configuration, model, run['initial']['score']])
-                for i, taken in enumerate(run['taken']):
-                    values.append([taken['ppl'], i+1, sut, configuration, model, taken['score']])
-    return pd.DataFrame(values, columns=['Perplexity', 'Iteration', 'SUT', 'Configuration', 'Model', 'score'])
+            if data['runs'][0]['taken'].__len__() > 1:
+                for run in data['runs']:
+                    values.append([run['initial']['ppl'], 0, sut, handle, model, run['initial']['score']])
+                    for i, taken in enumerate(run['taken']):
+                        values.append([taken['ppl'], i+1, sut, handle, model, taken['score']])
+            else:
+                handle='ppl_'+model
+                # case of baseline
+                for run in data['runs']:
+                    baselineValues.append([run['initial']['ppl'], 0, sut, handle, model, run['initial']['score']])
+    
+    return pd.DataFrame(values, columns=['Perplexity', 'Iteration', 'SUT', 'Handle', 'Model', 'score']), pd.DataFrame(baselineValues, columns=['Perplexity', 'Iteration', 'SUT', 'Handle', 'Model', 'score'])
 
-def plotPerplexity(data, output, folder, format='png'):
+def plotPerplexity(data, baseline, output, folder, format='png'):
     # plot the data
     # save the plot in output
-    modelNumber = len(data['Model'].unique())
+    modelNumber = len(data['SUT'].unique())
+    iterations=data['Iteration'].max()+1
+
+    # extend the baseline dataframe to have the same number of iterations as the data
+    for i in range(iterations):
+        newBaseline = baseline.copy()
+        newBaseline['Iteration'] = i
+        baseline = pd.concat([baseline, newBaseline])
+    
+    # merge the dataframes
+    data = pd.concat([data, baseline])
+
+
     # create a figure with as many subplots as models
 
     fig, axs = plt.subplots(modelNumber, 1, figsize=(12, 6*modelNumber))
-    sns.set_theme()
-    sns.set_context("paper")
-    sns.set_style("whitegrid")
-    for i, model in enumerate(data['Model'].unique()):
-        sns.lineplot(data=data[data['Model'] == model], x='Iteration', y='Perplexity', hue='SUT', style='Configuration', markers=True, dashes=False, ax=axs[i], errorbar=None)
-        axs[i].set_title(model)
+
+    # for each sut plot the perplexity
+    for i, sut in enumerate(data['SUT'].unique()):
+        sns.lineplot(data=data[data['SUT'] == sut], x='Iteration', y='Perplexity', hue='Handle', style='Handle', markers=True, dashes=False, ax=axs[i], errorbar=None)
+        axs[i].set_title(sut)
 
     plt.savefig(f'{folder}{output}.{format}', dpi=300)
 
@@ -57,27 +77,59 @@ def scorePlotter(data, output, folder, format='png'):
         print(data)
 
     for i, model in enumerate(data['Model'].unique()):
-        sns.lineplot(data=data[data['Model'] == model], x='Iteration', y='Ratio', hue='SUT', style='Configuration', markers=True, dashes=False, ax=axs[i], errorbar=None)
+        sns.lineplot(data=data[data['Model'] == model], x='Iteration', y='Ratio', hue='SUT', style='Handle', markers=True, dashes=False, ax=axs[i], errorbar=None)
         axs[i].set_title(model)
     
     plt.savefig(f'{folder}{output}_ratio.{format}', dpi=300)
 
-def normalisedScorePlotter(data, output, folder, format='png'):
+def normalisedScorePlotter(data, baseline, output, folder, format='png'):
     modelNumber = len(data['Model'].unique())
     # create a figure with as many subplots as models
     fig, axs = plt.subplots(modelNumber, 1, figsize=(12, 6*modelNumber))
     # normalise the score for each model and sut combination
     data['NormalisedScore'] = data.groupby(['Model', 'SUT'])['score'].transform(lambda x: (x-x.min())/(x.max()-x.min()))
+    baseline['NormalisedScore'] = baseline.groupby(['Model', 'SUT'])['score'].transform(lambda x: (x-x.min())/(x.max()-x.min()))
     # normalise the perplexity for each model and sut combination
     data['NormalisedPerplexity'] = data.groupby(['Model', 'SUT'])['Perplexity'].transform(lambda x: (x-x.min())/(x.max()-x.min()))
+    baseline['NormalisedPerplexity'] = baseline.groupby(['Model', 'SUT'])['Perplexity'].transform(lambda x: (x-x.min())/(x.max()-x.min()))
     # compute the ratio between the score and the perplexity
     data['Ratio'] = data['NormalisedScore']/data['NormalisedPerplexity']
+    baseline['Ratio'] = baseline['NormalisedScore']/baseline['NormalisedPerplexity']
     for i, model in enumerate(data['Model'].unique()):
         sns.lineplot(data=data[data['Model'] == model], x='Iteration', y='Ratio', hue='SUT', markers=True, dashes=False, ax=axs[i], errorbar=None)
         axs[i].set_title(model)
+        # plot an horizontal line for the baseline
+        baselinePpl = baseline[baseline['Model'] == model]
+        axs[i].axhline(y=baselinePpl['Ratio'].mean(), color='r', linestyle='--')
+
 
 
     plt.savefig(f'{folder}{output}_norm.{format}', dpi=300)
+
+
+def boxplot(data, baseline, output, folder, format='png'):
+    modelNumber = len(data['SUT'].unique())
+    # create a figure with as many subplots as models
+    fig, axs = plt.subplots(modelNumber, 1, figsize=(12, 6*modelNumber))
+    # plot the boxplots for each SUT
+    for i, model in enumerate(data['SUT'].unique()):
+        # create the dataframe, combining the baseline and the data
+        baselinePpl = baseline[baseline['SUT'] == model]
+        baselinePpl['Handle'] = 'JailbreakPrompt '
+        dataPpl = data[data['SUT'] == model]
+        dataPpl = pd.concat([dataPpl, baselinePpl])
+        sns.boxplot(data=dataPpl, x='Handle', y='Perplexity', ax=axs[i])
+        axs[i].set_title(model)
+        # set log scale on y
+        axs[i].set_yscale('log')
+
+        # print the data relative to the highest perplexity, file and model
+        print(f'{model} highest perplexity: {dataPpl["Perplexity"].max()}')
+        print(dataPpl[dataPpl['Perplexity'] == dataPpl['Perplexity'].max()])
+
+
+
+    plt.savefig(f'{folder}{output}_box.{format}', dpi=300)
 
 
 
@@ -88,9 +140,10 @@ def normalisedScorePlotter(data, output, folder, format='png'):
 
 def main(folder, output, format):
     perplexityFiles = getPerplexityFiles(folder)
-    data = sortValues(perplexityFiles)
-    #plotPerplexity(data, output, folder, format)
-    normalisedScorePlotter(data, output, folder, format)
+    data, baseline = sortValues(perplexityFiles)
+    plotPerplexity(data, baseline, output, folder, format)
+    #normalisedScorePlotter(data, baseline, output, folder, format)
+    boxplot(data, baseline, output, folder, format)
 
 if __name__ == '__main__':
     main()
--- tools/pplJsonFixer.py
+++ tools/pplJsonFixer.py
@@ -4,6 +4,10 @@
 import os
 import json
 import click
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from evolutionary import Archive, Config
+
 
 def getPerplexityFiles(root):
     # for each folder in folder
@@ -12,34 +16,45 @@ def getPerplexityFiles(root):
     perplexityFiles = []
     # folders in the root folder
     folders = [os.path.join(root, folder) for folder in os.listdir(root) if os.path.isdir(os.path.join(root, folder)) and folder != 'vicunaUC_vicunaUC']
-
     for folder in folders:
+        # furthermore, search if there are folders named "perplexity" and add the files in those folders
         perplexityFolder = os.path.join(folder, 'perplexity')
-        # list the folders in perplexityFolder
-        perpFolder= [os.path.join(perplexityFolder, folder) for folder in os.listdir(perplexityFolder) if os.path.isdir(os.path.join(perplexityFolder, folder))]
-        for model in perpFolder:
-            files = os.listdir(model)
+        # if the folder exists
+        if os.path.exists(perplexityFolder):
+            # list the folders in perplexityFolder
+            perpFolder= [os.path.join(perplexityFolder, folder) for folder in os.listdir(perplexityFolder) if os.path.isdir(os.path.join(perplexityFolder, folder))]
+            for model in perpFolder:
+                files = os.listdir(model)
+                for file in files:
+                    if file.startswith("ppl") and file.endswith(".json"):
+                        perplexityFiles.append(os.path.join(model, file))
+        else:
+            # if the folder does not exist, list the files in the folder
+            files = os.listdir(folder)
             for file in files:
                 if file.startswith("ppl") and file.endswith(".json"):
-                    perplexityFiles.append(os.path.join(model, file))
+                    perplexityFiles.append(os.path.join(folder, file))
     return perplexityFiles
 
 def fixJsonFiles(data):
+    #print(data)
     for file in data:
+        print(file)
         with open(file, 'r') as f:
             data = json.load(f)
             # get the name of the grandparent folder
             name = os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(file))))
-
-            
-            # pick the first part of the folder name
-            data['sut'] = name.split('_')[0]
-            # configuration field is the last part of the "results_file" field (last /), without the .json extension
-            data['configuration'] = os.path.basename(data['results_file']).split('.')[0]
+            # handle field is the last part of the "results_file" field (last /), without the .json extension
+            # an handle example is "max", "max-fs", etc
+            # could be inferred from the configuration file, but it is easier to get it from the results path
+            data['handle'] = os.path.basename(data['results_file']).split('.')[0]
             # load the corresponding json file, which is in the grandparent folder, named as the configuration
 
-            relativeFile=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(file))), data['configuration']+'.json')
-            print(relativeFile)
+            relativeFile=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(file))), data['handle']+'.json')
+            # load the relative file and extract the sut and configuration from there
+            with open(relativeFile, 'r') as f:
+                relativeArchive = Archive.from_dict(json.load(f))
+                data['config']=relativeArchive.config.to_dict()
 
             with open(relativeFile, 'r') as f:
                 relativeData = json.load(f)
