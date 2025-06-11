e4898c5915c73c350d4bb92329ee2263769d6ca2
Vincenzo Scotti <vincenzo.scotti@kit.edu>
2025-06-12 00:10:26 +0200

    Add script to automate AutoDAN experiments

---
+++ scripts/run_autodan_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set -xe
+
+# Declare files array
+models=(
+  "llama3"
+  "mistral"
+  "vicuna"
+  "vicunaUC"
+)
+
+# Change to AutoDAN repo
+cd submodules/AutoDAN
+# Loop over the tested models
+for model in "${models[@]}"; do
+  conda run -n toxic_oracles_autodan python autodan_hga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --num_steps 50
+  conda run -n toxic_oracles_autodan python get_responses.py --model "${model}" --path "./results/autodan_hga/"
+done
