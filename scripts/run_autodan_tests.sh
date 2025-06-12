35f11ecd4e5b9c03a5b5321dc95068d376560aa7
Vincenzo Scotti <vincenzo.scotti@kit.edu>
2025-06-12 22:50:47 +0200

    Update script for AutoDAN comparison experiments

--- scripts/run_autodan_tests.sh
+++ scripts/run_autodan_tests.sh
@@ -13,6 +13,5 @@ models=(
 cd submodules/AutoDAN
 # Loop over the tested models
 for model in "${models[@]}"; do
-  LOGLEVEL=DEBUG conda run -n toxic_oracles_autodan python autodan_hga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --num_steps 50
-  LOGLEVEL=DEBUG conda run -n toxic_oracles_autodan python get_responses.py --model "${model}" --path "./results/autodan_hga/"
+  conda run -n toxic_oracles_autodan python autodan_hga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --num_steps 50
 done
