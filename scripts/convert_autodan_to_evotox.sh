6d911fe15700412de235e7b4598185160a0282ed
Vincenzo Scotti <vincenzo.scotti@kit.edu>
2025-06-18 10:51:39 +0200

    Update conversion script

--- scripts/convert_autodan_to_evotox.sh
+++ scripts/convert_autodan_to_evotox.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 # set -xe
 
-# Declare files array
-input_files=(
-  "submodules/AutoDAN/results/autodan_ga/llama_0_llama.json"
-  "submodules/AutoDAN/results/autodan_ga/mistral_0_mistral.json"
-  "submodules/AutoDAN/results/autodan_ga/vicuna_0_vicuna.json",
-  "submodules/AutoDAN/results/autodan_ga/vicunaUC_0_vicunaUC.json"
-)
-
-output_files=(
-  "results/externalBaselines/llama3/autodan.json"
-  "results/externalBaselines/mistral/autodan.json"
-  "results/externalBaselines/vicuna/autodan.json"
-  "results/externalBaselines/vicunaUC/autodan.json"
-)
-
+# Declare data arrays
 model_ids=(
   "llama3"
   "mistral"
@@ -23,11 +9,7 @@ model_ids=(
   "vicunaUC"
 )
 
-# Loop over the input-output file pairs
-for i in "${!input_files[@]}"; do
-  input_file="${input_files[${i}]}"
-  output_file="${output_files[${i}]}"
-  model_id="${model_ids[${i}]}"
-
-  python tools/autodan_output_converter.py --input_path "${input_file}" --output_path "${output_file}" --model_id "${model_id}"
+# Loop over the models
+for model_id in "${model_ids[@]}"; do
+  echo python tools/autodan_output_converter.py --autodan_out_file_path "submodules/AutoDAN/results/autodan_hga/${model_id}_0_normal.json" --autodan_responses_file_path "submodules/AutoDAN/results/autodan_hga/${model_id}_0_normal_responses.json" --evotox_out_file_path "results/externalBaselines/${model_id}/autodan.json" --model_id "${model_id}"
 done
