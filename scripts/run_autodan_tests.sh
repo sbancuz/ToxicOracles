843befb1e8e9a233109b89ef4f45e2da98325764
Vincenzo Scotti <vincenzo.scotti@kit.edu>
2025-06-24 09:38:59 +0200

    Configure same budget of EvoTOx for AutoDAN

--- scripts/run_autodan_tests.sh
+++ scripts/run_autodan_tests.sh
@@ -13,8 +13,8 @@ models=(
 cd submodules/AutoDAN
 # Loop over the tested models
 for model in "${models[@]}"; do
-  # conda run -n toxic_oracles_autodan python autodan_ga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --save_suffix "${model}" --num_steps 50
+  # conda run -n toxic_oracles_autodan python autodan_ga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --save_suffix "${model}" --num_steps 10 --batch_size 6 --num_elites 0.167
   # conda run -n toxic_oracles_autodan LOG_LEVEL=DEBUG python get_responses.py --model "${model}" --path "./results/autodan_ga/${model}_0_${model}.json"
-  conda run -n toxic_oracles_autodan python autodan_hga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --num_steps 50
+  conda run -n toxic_oracles_autodan python autodan_hga_eval.py --model "${model}" --data "./data/EvoTox/questions_reduced100.csv" --num_steps 10 --batch_size 6 --num_elites 0.167
   conda run -n toxic_oracles_autodan LOG_LEVEL=DEBUG python get_responses.py --model "${model}" --path "./results/autodan_hga/${model}_0_normal.json"
 done
