fe0a505b022d347e2ab6ef17c5ab894bd24b8f7d
Vincenzo Scotti <vincenzo.scotti@kit.edu>
2025-06-25 20:35:07 +0200

    Fix perplexity configs reading

--- ngram_perplexity.py
+++ ngram_perplexity.py
@@ -59,7 +59,7 @@ def parse_config(data: Dict) -> Optional[Dict]:
     try:
         config = Archive.from_dict(data).config.to_dict()
     except ParseError:
-        config = None
+        config = data
 
     return config
 
